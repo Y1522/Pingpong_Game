b8fa24f2d2069c6afb40c189dace980b2b292be0
Youssef Sayed <youssefssayed5@gmail.com>
2025-09-11 23:58:43 +0300

    main.js

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ canvas.height = window.innerHeight;
 
 const paddleWidth = 18,
     paddleHeight = 120,
-    paddleSpeed = 8,
+    paddleSpeed = 5,
     ballRadius = 12,
-    initialBallSpeed = 16,
-    maxBallSpeed = 50,
+    initialBallSpeed = 8,
+    maxBallSpeed = 30,
     netWidth = 5,
     netColor = "Gray";
 
